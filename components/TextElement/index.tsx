624c62b568f337b58f6d30db2ebdb7a9d7939375
Melvin Mathew <36991926+melvingm101@users.noreply.github.com>
2026-09-10 21:59:29 +0400

    fix: fixing the test cases

--- components/TextElement/index.tsx
+++ components/TextElement/index.tsx
@@ -2,7 +2,9 @@ import { WebsiteElement } from "@/types/element";
 
 const TextElement = ({ element }: { element: WebsiteElement }) => {
   return (
-    <div id={element.id} className={element.className}>{element.text}</div>
+    <div id={element.id} className={element.className} data-testid={element.id}>
+      {element.text}
+    </div>
   )
 }
 
